9bb27832361ac317a92a471ddb1100d9b72d3cf0
Kristian Lalov <lalov.kr@gmail.com>
2022-11-17 00:18:21 +0200

    created post model

---
+++ JSWeb-exams/Wildlife-Photography/models/Post.js
@@ -0,0 +1,28 @@
+const { Schema, model, Types } = require('mongoose');
+
+const URL_PATTERN = /^https?:\/\/.+$/i;
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  keyword: { type: String, required: true },
+  location: { type: String, required: true },
+  date: { type: String, required: true },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        URL_PATTERN.test(value);
+      },
+      message: 'Invalid image url!',
+    },
+  },
+  description: { type: String, required: true },
+  author: { type: Types.ObjectId, ref: 'User' },
+  votes: { type: [Types.ObjectId], ref: 'User', default: [] },
+  rating: { type: Number, default: 0 },
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
