c78afee25eb0b8bba2d126b62e6144fd7b83e50d
Kristian Lalov <lalov.kr@gmail.com>
2022-11-20 23:45:50 +0200

    finished assignment

--- Programming Fundamentals/Exam Preperations/Problem 3 - The Pianist.js
+++ Programming Fundamentals/Exam Preperations/Problem 3 - The Pianist.js
@@ -12,30 +12,41 @@ function solve(arr) {
     };
   }
 
-  console.log(arr);
+  //console.log(arr);
 
   while (arr[0] != 'Stop') {
     let [command, pieceName, param2, param3] = arr.shift().split('|');
 
     if (command == 'Add') {
       if (pieces[pieceName]) {
-        console.log(`${pieceName} is already in the collection!
-        `);
+        console.log(`${pieceName} is already in the collection!`);
         continue;
       }
-
       pieces[pieceName] = {
         param2,
         param3,
       };
-
       console.log(
-        `${pieceName} by ${param2} in ${param3} added to the collection!`
-      );
+        `${pieceName} by ${param2} in ${param3} added to the collection!`);
+    } else if (command == 'Remove') {
+      if (pieces[pieceName]) {
+        delete pieces[pieceName];
+        console.log(`Successfully removed ${pieceName}!`);
+      } else {
+        console.log(
+          `Invalid operation! ${pieceName} does not exist in the collection.`);
+      }
+    } else if (command == 'ChangeKey') {
+      if (pieces[pieceName]) {
+        pieces[pieceName] = pieces[param2];
+        console.log(`Changed the key of ${pieceName} to ${param2}!`);
+      } else {
+        console.log(`Invalid operation! ${pieceName} does not exist in the collection.`);
+      }
     }
   }
 
-  console.log(pieces);
+
 }
 solve([
   '3',
