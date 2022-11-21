92bd16020fbe5a65267752e3e411a5842b51cb6c
Kristian Lalov <lalov.kr@gmail.com>
2022-11-22 01:24:18 +0200

    finished assignment

--- Js Advanced/Js Advanced Exams/01. Dish Manager_Resources/app.js
+++ Js Advanced/Js Advanced Exams/01. Dish Manager_Resources/app.js
@@ -6,7 +6,7 @@ function solve() {
     lastName: document.getElementById('last-name'),
     age: document.getElementById('age'),
     gender: document.getElementById('genderSelect'),
-    task: document.getElementById('task'),
+    description: document.getElementById('task'),
   };
 
   const lists = {
@@ -21,67 +21,79 @@ function solve() {
 
   function submitForm(event) {
     event.preventDefault();
-    const firstName = inputs.firstName.value
-    const lastName = inputs.lastName.value
-    const gender = inputs.gender.value
-    const age = inputs.age.value
-    const task =  inputs.task.value
+    const firstName = inputs.firstName.value;
+    const lastName = inputs.lastName.value;
+    const gender = inputs.gender.value;
+    const age = inputs.age.value;
+    const description = inputs.description.value;
+
+    if (firstName == '' || lastName == '' || description == '' || age == '') {
+      return;
+    }
+
     const li = document.createElement('li');
-    li.classList.add('each-line');
+    li.className = 'each-line';
     const articleEl = document.createElement('article');
     const h4 = document.createElement('h4');
     const p1 = document.createElement('p');
-    const p2 = document.createElement('p2');
+    const p2 = document.createElement('p');
     const editBtn = document.createElement('button');
     editBtn.textContent = 'Edit';
     editBtn.classList.add('edit-btn');
     const completeBtn = document.createElement('button');
     completeBtn.textContent = 'Mark as Complete';
     completeBtn.classList.add('complete-btn');
-
     h4.textContent = `${firstName} ${lastName}`;
     p1.textContent = `${gender}, ${age}`;
-    p2.textContent = task
+    p2.textContent = `Dish description: ${description}`;
 
-    const elArr = [h4, p1, p2];
-    elArr.forEach((x) => articleEl.appendChild(x));
+    articleEl.appendChild(h4);
+    articleEl.appendChild(p1);
+    articleEl.appendChild(p2);
+
     li.appendChild(articleEl);
     li.appendChild(editBtn);
     li.appendChild(completeBtn);
 
     lists.inProgress.appendChild(li);
+
+    inputs.firstName.value = '';
+    inputs.lastName.value = '';
+    inputs.age.value = '';
+    inputs.description.value = '';
+
     count++;
     counter.textContent = count;
 
     completeBtn.addEventListener('click', completeTask);
-    editBtn.addEventListener("click", edit )
-    function completeTask() {
+    editBtn.addEventListener('click', edit);
+
+    function completeTask(ev) {
+      ev.preventDefault();
       lists.finished.appendChild(li);
       editBtn.remove();
       completeBtn.remove();
       count--;
-
       counter.textContent = count;
     }
 
-    inputs.firstName.value = ' ';
-    inputs.lastName.value = ' ';
-    inputs.task.value = ' ';
+    function edit(ev) {
+      ev.preventDefault();
+      inputs.firstName.value = firstName;
+      inputs.lastName.value = lastName;
+      inputs.gender.value = gender;
+      inputs.description.value = description;
+      inputs.age.value = age;
 
-    function edit() {
-      inputs.firstName.value = firstName
-      inputs.lastName.value = lastName
-      inputs.gender.value = gender
-      inputs.task.value = task
-      inputs.age.value = age
       count--;
-
       counter.textContent = count;
-        li.remove();
+
+      li.remove();
     }
   }
 
-  function clearBox() {
-    lists.finished.innerHTML = ' ';
+  function clearBox(ev) {
+    ev.preventDefault();
+    lists.finished.innerHTML = '';
   }
 }
