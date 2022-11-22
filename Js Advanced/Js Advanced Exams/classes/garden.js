db1862be2e69b463c0ff8e78804048f00427de6f
Kristian Lalov <lalov.kr@gmail.com>
2022-11-22 20:30:37 +0200

    Added ripe function

--- Js Advanced/Js Advanced Exams/classes/garden.js
+++ Js Advanced/Js Advanced Exams/classes/garden.js
@@ -23,5 +23,43 @@ class Garden {
     return `The ${plantName} has been successfully planted in the garden.`;
   }
 
+  ripenPlant(plantName, quantity) {
+    const plant = this.plants.find((p) => p.plantName == plantName);
+
+    if (!this.plants.includes(plant[plantName])) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+
+    if (plant[plantName].ripe == true) {
+      throw new Error(`The ${plantName} is already ripe.`);
+    }
+
+    if (quantity <= 0) {
+      throw new Error('The quantity cannot be zero or ngeative.');
+    }
+
+    plant[plantName].ripe = true;
+
+    if (quantity == 1) {
+      return `${quantity} ${plantName} has successfully ripened.`;
+    } else {
+      return `${quantity} ${plantName}s have successfully ripened.`;
+    }
+  }
+
+  harvestPlant(plantName){ 
+    const plant = this.plants.find((p) => p.plantName == plantName);
+
+    if (!this.plants.includes(plant[plantName])) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+
+    if(plant[plantName].ripe == false){ 
+        throw new Error(`The ${plantName} cannot be harvested before it is ripe.`)
+    }
+
     
+  }
+
+
 }
