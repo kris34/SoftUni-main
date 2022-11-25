f7a0f4605fdc138c8c963beee0116b0d79fb810e
Kristian Lalov <lalov.kr@gmail.com>
2022-11-25 18:45:14 +0200

    created login view

--- JS Applications-exams/Meme Lounge/src/app.js
+++ JS Applications-exams/Meme Lounge/src/app.js
@@ -1,6 +1,7 @@
 import { page, render } from './lib.js';
 import { catalogView } from './views/catalog.js';
 import { homeView } from './views/home.js';
+import { loginView } from './views/login.js';
 
 const main = document.querySelector('main');
 
@@ -9,7 +10,7 @@ page('/', homeView);
 page('/memes', catalogView);
 page('/memes/:id', () => console.log('details'));
 page('/edit/:id', () => console.log('edit'));
-page('/login', () => console.log('login'));
+page('/login', loginView);
 page('/register', () => console.log('register'));
 page('/create', () => console.log('create'));
 page('/profile', () => console.log('profile'));
