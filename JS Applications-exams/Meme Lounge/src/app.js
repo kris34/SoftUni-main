b606be6da1da19b550d91e973dafd9b2309133ec
Kristian Lalov <lalov.kr@gmail.com>
2022-11-25 17:40:22 +0200

    implemented homeView

--- JS Applications-exams/Meme Lounge/src/app.js
+++ JS Applications-exams/Meme Lounge/src/app.js
@@ -1,6 +1,10 @@
 import { page, render } from './lib.js';
+import { homeView } from './views/home.js';
 
-page('/', () => console.log('home'));
+const main = document.querySelector('main');
+
+page(decorateContext);
+page('/', homeView);
 page('/memes', () => console.log('catalog'));
 page('/memes/:id', () => console.log('details'));
 page('/edit/:id', () => console.log('edit'));
@@ -10,3 +14,12 @@ page('/create', () => console.log('create'));
 page('/profile', () => console.log('profile'));
 
 page.start();
+
+function decorateContext(ctx, next) {
+  ctx.render = renderMain;
+  next();
+}
+
+function renderMain(templateResult) {
+  render(templateResult, main);
+}
