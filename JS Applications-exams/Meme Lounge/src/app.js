693bdbea339f5a1f921238816cdaa5f7350d3ddd
Kristian Lalov <lalov.kr@gmail.com>
2022-11-25 17:19:33 +0200

    full skeleton ready

--- JS Applications-exams/Meme Lounge/src/app.js
+++ JS Applications-exams/Meme Lounge/src/app.js
@@ -1,3 +1,12 @@
-import * as api from './api/users.js';
+import { page, render } from './lib.js';
 
-window.api = api;
+page('/', () => console.log('home'));
+page('/memes', () => console.log('catalog'));
+page('/memes/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+page('/profile', () => console.log('profile'));
+
+page.start();
