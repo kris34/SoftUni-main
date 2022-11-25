938506399713493e99c8ca534c19713d861d1384
Kristian Lalov <lalov.kr@gmail.com>
2022-11-25 21:19:46 +0200

    implemented working create meme

--- JS Applications-exams/Meme Lounge/src/api/memes.js
+++ JS Applications-exams/Meme Lounge/src/api/memes.js
@@ -1,5 +1,9 @@
-import { get } from './api.js';
+import { get, post } from './api.js';
 
 export async function getAllMemes() {
   return get('/data/memes?sortBy=_createdOn%20desc');
+}
+
+export async function createMeme(meme) {
+  return post('/data/memes', meme);
 }
--- JS Applications-exams/Meme Lounge/src/views/createView.js
+++ JS Applications-exams/Meme Lounge/src/views/createView.js
@@ -1,22 +1,51 @@
+import { createMeme } from '../api/memes.js';
 import { html } from '../lib.js';
 
-const createTemplate = () => html`
-<section id="create-meme">
-<form id="create-form">
-    <div class="container">
+const createTemplate = (onSubmit) => html`
+  <section id="create-meme">
+    <form @submit=${onSubmit} id="create-form">
+      <div class="container">
         <h1>Create Meme</h1>
         <label for="title">Title</label>
-        <input id="title" type="text" placeholder="Enter Title" name="title">
+        <input id="title" type="text" placeholder="Enter Title" name="title" />
         <label for="description">Description</label>
-        <textarea id="description" placeholder="Enter Description" name="description"></textarea>
+        <textarea
+          id="description"
+          placeholder="Enter Description"
+          name="description"
+        ></textarea>
         <label for="imageUrl">Meme Image</label>
-        <input id="imageUrl" type="text" placeholder="Enter meme ImageUrl" name="imageUrl">
-        <input type="submit" class="registerbtn button" value="Create Meme">
-    </div>
-</form>
-</section>
+        <input
+          id="imageUrl"
+          type="text"
+          placeholder="Enter meme ImageUrl"
+          name="imageUrl"
+        />
+        <input type="submit" class="registerbtn button" value="Create Meme" />
+      </div>
+    </form>
+  </section>
 `;
 
 export function createView(ctx) {
-  ctx.render(createTemplate());
+  ctx.render(createTemplate(onSubmit));
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const meme = {
+      title: formData.get('title'),
+      description: formData.get('description'),
+      imageUrl: formData.get('imageUrl'),
+    };
+
+    if (meme.title == '' || meme.description == '' || imageUrl == '') {
+      return alert('All fields are required!');
+    }
+    await createMeme(meme);
+    event.target.reset();
+    ctx.page.redirect('/memes');
+  }
 }
