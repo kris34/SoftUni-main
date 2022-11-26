e0a3ce48523ce84018aabb8174c5dcad31b36ae7
Kristian Lalov <lalov.kr@gmail.com>
2022-11-26 18:14:18 +0200

    static home page and fixed background image

--- JSWeb-exams/Shared-Trip/controllers/home.js
+++ JSWeb-exams/Shared-Trip/controllers/home.js
@@ -1,7 +1,10 @@
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    title: 'Home Page',
+    
+  });
 });
 
 module.exports = homeController;
