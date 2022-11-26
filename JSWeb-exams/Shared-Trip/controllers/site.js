3db7c1bcb8331265c16b349aa4784f5fd0e3fcd8
Kristian Lalov <lalov.kr@gmail.com>
2022-11-27 00:50:20 +0200

    Made some validations on trip creation

--- JSWeb-exams/Shared-Trip/controllers/site.js
+++ JSWeb-exams/Shared-Trip/controllers/site.js
@@ -1,4 +1,5 @@
 const createTrip = require('../services/siteService');
+const { parseError } = require('../util/parser');
 
 const siteController = require('express').Router();
 
@@ -21,8 +22,31 @@ siteController.post('/create', async (req, res) => {
     description: req.body.description,
     creator: req.user?._id?.toString(),
   };
-  const trip = await createTrip(data);
-  res.redirect('/');
+
+  try {
+    if (Object.values(data).some((v) => !v)) {
+      throw new Error('All fields required!');
+    }
+
+    if (Number(data.seats) < 0 || Number(data.seats) > 4) {
+      throw new Error('Seats cannot be less then 0 or more then 4');
+    }
+
+    if (Number(data.price) < 0) {
+      throw new Error('Price cannot be lower then $0');
+    }
+
+    await createTrip(data);
+    
+    res.redirect('/');
+  } catch (err) {
+    const errors = parseError(err);
+    res.render('trip-create', {
+      title: 'Create',
+      errors,
+      data,
+    });
+  }
 });
 
 module.exports = siteController;
