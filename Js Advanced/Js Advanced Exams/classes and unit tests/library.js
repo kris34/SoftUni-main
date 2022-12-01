3f3623c08468023cddabb79a775ff428be960bc9
Kristian Lalov <lalov.kr@gmail.com>
2022-12-01 13:15:57 +0200

    implemented addBook function

--- Js Advanced/Js Advanced Exams/classes and unit tests/library.js
+++ Js Advanced/Js Advanced Exams/classes and unit tests/library.js
@@ -1,5 +1,21 @@
-class LibraryCollection { 
-    constructor(){ 
-        
+class LibraryCollection {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.books = [];
+  }
+
+  addBook(bookName, bookAuthor) {
+    if (this.books.length >= this.capacity) {
+      throw new Error('Not enough space in the collection.');
     }
+
+    this.books.push({ bookName, bookAuthor, payed: false });
+    return `The ${bookName}, with an author ${bookAuthor}, collect.`;
+  }
+
+  
 }
+const library = new LibraryCollection(2);
+console.log(library.addBook('In Search of Lost Time', 'Marcel Proust'));
+console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
+console.log(library.addBook('Ulysses', 'James Joyce'));
