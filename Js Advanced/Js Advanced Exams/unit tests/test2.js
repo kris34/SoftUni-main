7d3999f122b1046901988e65f0c937bb54d6d428
Kristian Lalov <lalov.kr@gmail.com>
2022-12-02 00:52:18 +0200

    calcPriceOfFlowers tests working

---
+++ Js Advanced/Js Advanced Exams/unit tests/test2.js
@@ -0,0 +1,38 @@
+const { flowerShop } = require('./solution2');
+const { expect } = require('chai');
+
+describe('Test', () => {
+  describe('calcPriceOfFlowers', () => {
+    it('Happy path', () => {
+      expect(flowerShop.calcPriceOfFlowers('Rose', 2, 2)).to.equal(
+        `You need $4.00 to buy Rose!`
+      );
+    });
+
+    it('Wrong input', () => {
+      expect(() => flowerShop.calcPriceOfFlowers(1, 'a', 'b')).to.throw();
+    });
+  });
+
+  describe('checkFlowersAvailable', () => {
+    it('includes All', () => {
+      expect(
+        flowerShop.checkFlowersAvailable('rose', ['rose', 'lily'])
+      ).to.equal('The rose are available!');
+    });
+
+    it('doesnt incluse', () => {
+      expect(flowerShop.checkFlowersAvailable('rose', ['lili'])).to.equal(
+        'The rose are sold! You need to purchase more!'
+      );
+    });
+  });
+
+  describe("sellFlowers", () => { 
+    it("works", () => { 
+        expect(flowerShop.sellFlowers(["Rose", "Lily", "Orchid"], 1)).to.equal("Rose / Orchid")
+    })
+  })
+
+  
+});
---
+++ Js Advanced/Js Advanced Exams/unit tests/test6.js
@@ -0,0 +1,1 @@
+console.log(["Rose", "Lily", "Orchid"].join("/"));
