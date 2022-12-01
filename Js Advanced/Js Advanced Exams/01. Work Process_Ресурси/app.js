ffda3f7c38769712cd536abaaeef3db98df865e7
Kristian Lalov <lalov.kr@gmail.com>
2022-12-02 01:08:49 +0200

    got all input fields from the form

--- Js Advanced/Js Advanced Exams/01. Work Process_Ресурси/app.js
+++ Js Advanced/Js Advanced Exams/01. Work Process_Ресурси/app.js
@@ -1,4 +1,19 @@
 function solve() {
-    //TODO
+  let inputs = {
+    firstName: document.getElementById('fname'),
+    lastName: document.getElementById('lname'),
+    email: document.getElementById('email'),
+    birth: document.getElementById('birth'),
+    position: document.getElementById('position'),
+    salary: document.getElementById('salary'),
+  };
+
+  let count = 0;
+  let counter = document.getElementById('sum');
+
+  function submitForm(event){ 
+    
+  }
+  
 }
-solve()
+solve();
