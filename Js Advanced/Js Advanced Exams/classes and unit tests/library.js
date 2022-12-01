9068bbf04f1b2f9da10c136c4d28150fa5d8236a
Kristian Lalov <lalov.kr@gmail.com>
2022-12-01 15:34:49 +0200

    payBook function implemented

--- Js Advanced/Js Advanced Exams/classes and unit tests/library.js
+++ Js Advanced/Js Advanced Exams/classes and unit tests/library.js
@@ -13,9 +13,25 @@ class LibraryCollection {
     return `The ${bookName}, with an author ${bookAuthor}, collect.`;
   }
 
+  payBook(bookName) {
+    let isFound = this.books.some((b) => b.bookName == bookName);
   
+    if (isFound == false) {
+      throw new Error(`${bookName} is not in the collection.`);
+    }
+
+    let isPayed = this.books.some((b) => b.payed == true);
+
+    if (isPayed == true) {
+      throw new Error(`${bookName} has already been paid.`);
+    } else {
+      let book = this.books.find((b) => b.bookName == bookName);
+      book.payed = true;
+      return `${bookName} has been successfully paid.`;
+    }
+  }
 }
 const library = new LibraryCollection(2);
-console.log(library.addBook('In Search of Lost Time', 'Marcel Proust'));
-console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
-console.log(library.addBook('Ulysses', 'James Joyce'));
+library.addBook('In Search of Lost Time', 'Marcel Proust');
+console.log(library.payBook('In Search of Lost Time'));
+console.log(library.payBook('Don Quixote'));
