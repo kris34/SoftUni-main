8f6bb398cdbc8b1936d93347c7191a7e6c9b9032
Kristian Lalov <lalov.kr@gmail.com>
2022-12-06 16:07:32 +0200

    created showMenu function

--- Js Advanced/Js Advanced Exams/classes and unit tests/restaurant.js
+++ Js Advanced/Js Advanced Exams/classes and unit tests/restaurant.js
@@ -50,8 +50,6 @@ class Restaurant {
       };
     }
 
-    console.log(this.menu);
-
     if (Object.keys(this.menu).length == 1) {
       return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
     } else {
@@ -61,9 +59,19 @@ class Restaurant {
     }
   }
 
-  
+  showTheMenu() {
+    let menuEntries = Object.entries(this.menu);
+    if (menuEntries.length == 0) {
+      return 'Our menu is not ready yet, please come later...';
+    } else {
+      let result = [];
+      for (let entry of menuEntries) {
+        result.push(`${entry[0]} - $ ${entry[1].price}`);
+      }
+      return result.join('\n');
+    }
+  }
 }
-
 let kitchen = new Restaurant(1000);
 console.log(
   kitchen.addToMenu(
@@ -88,19 +96,5 @@ console.log(
     15.55
   )
 );
-console.log(
-  kitchen.addToMenu(
-    'Pizza',
-    [
-      'Flour 0.5',
-      'Oil 0.2',
-      'Yeast 0.5',
-      'Salt 0.1',
-      'Sugar 0.1',
-      'Tomato sauce 0.5',
-      'Pepperoni 1',
-      'Cheese 1.5',
-    ],
-    15.55
-  )
-);
+
+console.log(kitchen.showTheMenu());
