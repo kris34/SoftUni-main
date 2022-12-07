31ab48c846fd143e2c280c669a87185b28c8a5bc
Kristian Lalov <lalov.kr@gmail.com>
2022-12-07 09:11:19 +0200

    created inputs object and submit function

---
+++ Js Advanced/js advanced exam/app.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', solve);
+
+function solve() {
+  let inputs = {
+    firstName: document.getElementById('first-name'),
+    lastName: document.getElementById('last-name'),
+    checkIn: document.getElementById('date-in'),
+    checkOut: document.getElementById('check-out'),
+    guestsNum: document.getElementById('people-count'),
+  };
+
+  document.getElementById('submit').addEventListener('click', onSubmit);
+
+  function onSubmit(){ 
+    
+  }
+
+
+}
