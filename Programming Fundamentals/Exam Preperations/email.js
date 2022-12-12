3c9f4868228e59ec2a531cffd971a70fad4424fa
Kristian Lalov <lalov.kr@gmail.com>
2022-12-12 14:06:56 +0200

    finished assignment

--- Programming Fundamentals/Exam Preperations/email.js
+++ Programming Fundamentals/Exam Preperations/email.js
@@ -1,7 +1,31 @@
 function solve(input) {
   let email = input.shift();
 
-  // while (input[0] != 'Complete') {}
+  while (input[0] != 'Complete') {
+    let tokens = input.shift().split(' ');
+
+    if (tokens[1] == 'Upper') {
+      email = makeUpper(email);
+      console.log(email);
+    } else if (tokens[1] == 'Lower') {
+      email = makeLower(email);
+      console.log(email);
+    } else if (tokens[0] == 'GetDomain') {
+      let domain = getDomain(email, Number(tokens[1]));
+      console.log(domain);
+    } else if (tokens[0] == 'GetUsername') {
+      if (!email.includes('@')) {
+        console.log(`The email ${email} doesn't contain the @ symbol.`);
+      } else {
+        console.log(getUsername(email));
+      }
+    } else if (tokens[0] == 'Replace') {
+      email = replace(email, tokens[1]);
+      console.log(email);
+    } else if (tokens[0] == 'Encrypt') {
+      console.log(encrypt(email));
+    }
+  }
 
   function makeUpper(string) {
     return string.toUpperCase();
@@ -24,6 +48,7 @@ function solve(input) {
         result += char;
       }
     }
+    return result;
   }
 
   function replace(string, char) {
@@ -40,7 +65,13 @@ function solve(input) {
     return result;
   }
 
-  console.log(replace(email, 'i'));
+  function encrypt(string) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+      result += `${string.charCodeAt(i)} `;
+    }
+    return result;
+  }
 }
 solve([
   'Mike123@somemail.com',
