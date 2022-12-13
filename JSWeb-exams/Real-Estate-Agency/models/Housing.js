7dfed1ab9a40bfebe47b847b7489cb21d7c5aa2d
Kristian Lalov <lalov.kr@gmail.com>
2022-12-13 16:58:59 +0200

    created housing model and create view

---
+++ JSWeb-exams/Real-Estate-Agency/models/Housing.js
@@ -0,0 +1,17 @@
+const { Schema, model, Types } = require('mongoose');
+
+const housingSchema = {
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  year: { type: String, required: true },
+  city: { type: String, required: true },
+  image: { type: String, required: true },
+  description: { type: String, required: true },
+  availablePieces: { type: Number, required: true },
+  rented: { type: [Types.ObjectId], ref: 'User', default: [] },
+  owner: { type: Types.ObjectId, ref: 'User' },
+};
+
+const Housing = model('Housing', housingSchema);
+
+module.exports = Housing;
