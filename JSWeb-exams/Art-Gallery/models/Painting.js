6c3b76c0d5cc8bb65dc97b7cbbb2fa708729e87e
Kristian Lalov <lalov.kr@gmail.com>
2022-12-18 21:44:39 +0200

    implemented painting schema

---
+++ JSWeb-exams/Art-Gallery/models/Painting.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require('mongoose');
+
+const paintingSchema = new Schema({
+  title: { type: String, required: true },
+  technique: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  certificate: { type: String, required: true },
+  author: { type: Types.ObjectId, ref: 'User' },
+  shared: { type: [Types.ObjectId], ref: 'User', default: [] },
+});
+
+const Painting = new model('Painting', paintingSchema);
+
+module.exports = paintingSchema;
