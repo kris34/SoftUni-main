9fa1ded3cad152e888f752bfd6a92200237887e8
Kristian Lalov <lalov.kr@gmail.com>
2022-12-18 18:09:28 +0200

    dynamic userNav

--- JSWeb-exams/Art-Gallery/middlewares/session.js
+++ JSWeb-exams/Art-Gallery/middlewares/session.js
@@ -6,6 +6,7 @@ module.exports = () => (req, res, next) => {
   if (token) {
     try {
       const userData = verifyToken(token);
+      res.locals.username = userData.username;
       req.user = userData;
     } catch (err) {
       res.clearCookie('token');
