e7281d5f35ed90e23ba2b70adf7d7a8fe7286044
Kristian Lalov <lalov.kr@gmail.com>
2022-12-19 22:17:30 +0200

    implemented dynamic home

--- JSWeb-exams/Art-Gallery/controllers/home.js
+++ JSWeb-exams/Art-Gallery/controllers/home.js
@@ -1,8 +1,13 @@
+const { getAll } = require('../services/siteService');
+
 const homeController = require('express').Router();
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
+  const paintings = await getAll();
+
   res.render('home', {
     title: 'home',
+    paintings,
   });
 });
 
