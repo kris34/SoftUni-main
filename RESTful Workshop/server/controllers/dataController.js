d45d692abf740f969c546a3dc61992b58a7adad0
Kristian Lalov <lalov.kr@gmail.com>
2023-01-01 21:02:34 +0200

    Finished the project, fully working RESTful API

--- RESTful Workshop/server/controllers/dataController.js
+++ RESTful Workshop/server/controllers/dataController.js
@@ -21,8 +21,8 @@ dataController.post('/', hasUser(), async (req, res) => {
         const data = Object.assign({ _ownerId: req.user._id }, req.body);
         const item = await create(data);
         res.json(item);
-    } catch (err) {
-        const message = parseError(err);
+    } catch (errors) {
+        const message = parseError(errors);
         res.status(400).json({ message });
     }
 });
