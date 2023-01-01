ec285a494a7874a3478de4c3d11a3c650923fe27
Kristian Lalov <lalov.kr@gmail.com>
2023-01-01 16:19:19 +0200

    working edit functionality

--- RESTful Workshop/server/controllers/dataController.js
+++ RESTful Workshop/server/controllers/dataController.js
@@ -1,5 +1,5 @@
 const { hasUser } = require('../middlewares/guards');
-const { getAll, create, getById } = require('../services/itemService');
+const { getAll, create, getById, update } = require('../services/itemService');
 const { parseError } = require('../util/parser');
 
 const dataController = require('express').Router();
@@ -26,4 +26,29 @@ dataController.get('/:id', async (req, res, next) => {
   res.json(item);
 });
 
+dataController.put('/:id', async (req, res, next) => {
+  const item = await getById(req.params.id);
+
+  if (req.user._id != item._ownerId) {
+    return res.status(403).json({ message: 'You cannot modify this record' });
+  }
+
+  try {
+    const result = await update(req.params.id, req.body);
+    res.json(result);
+  } catch (err) {
+    const message = parseError(err);
+    res.status(400).json({ message });
+  }
+});
+
+dataController.delete('/:id', async (req, res) => {
+  const item = await getById(req.params.id);
+
+  if (req.user._id != item._ownerId) {
+    return res.status(403).json({ message: 'You cannot modify this record' });
+  }
+
+  
+});
 module.exports = dataController;
