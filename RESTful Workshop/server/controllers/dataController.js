5ee155c94f746b7ac3848c15b7c9325854162169
Kristian Lalov <lalov.kr@gmail.com>
2023-01-01 16:04:33 +0200

    create item works properly

--- RESTful Workshop/server/controllers/dataController.js
+++ RESTful Workshop/server/controllers/dataController.js
@@ -1,8 +1,22 @@
+const { getAll, create } = require('../services/itemService');
+const { parseError } = require('../util/parser');
+
 const dataController = require('express').Router();
 
-dataController.get('/', (req, res) => {
-  console.log(req.user);
-  res.json([]);
+dataController.get('/', async (req, res) => {
+  const items = await getAll();
+  res.json(items);
+});
+
+dataController.post('/', async (req, res) => {
+  try {
+    const data = Object.assign({ _ownerId: req.user._id }, req.body);
+    const item = await create(data);
+    res.json(item);
+  } catch (err) {
+    const message = parseError(err);
+    res.status(400).json({ message });
+  }
 });
 
 module.exports = dataController;
