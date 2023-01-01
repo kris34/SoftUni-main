12c35628a5e5bf805bd6f0040d488cd4c132413a
Kristian Lalov <lalov.kr@gmail.com>
2023-01-01 14:48:08 +0200

    added parseError function

--- RESTful Workshop/server/controllers/authController.js
+++ RESTful Workshop/server/controllers/authController.js
@@ -1,17 +1,31 @@
 const { register, login } = require('../services/userService');
+const { body, validationResult } = require('express-validator');
 
 const authController = require('express').Router();
 
-authController.post('/register', async (req, res) => {
-  try {
-    const token = await register(req.body.email, req.body.password);
-    res.json(token);
-  } catch (error) {
-    res.status(400).json({
-      message: error.message,
-    });
+authController.post(
+  '/register',
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password')
+    .isLength({ min: 3 })
+    .withMessage('Password must be at least 3 charakters long!'),
+  async (req, res) => {
+    try {
+      const { errors } = validationResult(req);
+
+      if (errors.length > 0) {
+        throw errors;
+      }
+
+      const token = await register(req.body.email, req.body.password);
+      res.json(token);
+    } catch (error) {
+      res.status(400).json({
+        message: error.message,
+      });
+    }
   }
-});
+);
 
 authController.post('/login', async (req, res) => {
   try {
