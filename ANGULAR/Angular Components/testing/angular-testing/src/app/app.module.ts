2998393877c0eabd8fe7470dae325cdc28fde5c2
Kristian Lalov <lalov.kr@gmail.com>
2023-01-04 14:30:19 +0200

    added car component

--- ANGULAR/Angular Components/testing/angular-testing/src/app/app.module.ts
+++ ANGULAR/Angular Components/testing/angular-testing/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
