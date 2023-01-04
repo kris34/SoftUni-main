5040ca730ad77e7677b39cb922d321548ace5926
Kristian Lalov <lalov.kr@gmail.com>
2023-01-04 14:29:50 +0200

    deleted test folders to start new better one

--- ANGULAR/Angular Components/testing/angular-testing/src/app/app.module.ts
+++ ANGULAR/Angular Components/testing/angular-testing/src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
