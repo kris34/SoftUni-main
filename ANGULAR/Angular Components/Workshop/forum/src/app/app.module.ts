2ec50f5ebb61a7088201f0242f8b60505f5b9963
Kristian Lalov <lalov.kr@gmail.com>
2023-01-06 23:06:40 +0200

    added environments folder

--- ANGULAR/Angular Components/Workshop/forum/src/app/app.module.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecentPostsComponent, ThemeListComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
