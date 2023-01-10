e0cb1812cbf44df33160f3f071db61f9b1b82978
Kristian Lalov <lalov.kr@gmail.com>
2023-01-10 12:32:17 +0200

    added routing to main component

--- ANGULAR/Modules and Routing/routing/routing/src/app/core/main/main.component.ts
+++ ANGULAR/Modules and Routing/routing/routing/src/app/core/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
 
 
 export class MainComponent {
-
+constructor(private router: Router){ 
+  
+}
 }
