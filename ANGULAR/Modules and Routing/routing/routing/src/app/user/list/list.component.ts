f9d5b6d979faa6256b8c7ff19297a71b4dc61b1c
Kristian Lalov <lalov.kr@gmail.com>
2023-01-10 12:27:30 +0200

    implemented simple userlist logic to show names

--- ANGULAR/Modules and Routing/routing/routing/src/app/user/list/list.component.ts
+++ ANGULAR/Modules and Routing/routing/routing/src/app/user/list/list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/interfaces/user';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
+export class ListComponent implements OnInit {
+  users: IUser[] | null = null;
 
+  constructor(private userApi: UserServiceService) {}
 
-export class ListComponent {
-
+  ngOnInit(): void {
+    this.userApi.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+    });
+  }
 }
