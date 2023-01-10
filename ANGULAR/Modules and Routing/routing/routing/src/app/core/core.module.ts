be239fa739faf39c265c0f299b9626d8441d4064
Kristian Lalov <lalov.kr@gmail.com>
2023-01-10 12:02:57 +0200

    added main component to app routing

--- ANGULAR/Modules and Routing/routing/routing/src/app/core/core.module.ts
+++ ANGULAR/Modules and Routing/routing/routing/src/app/core/core.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ]
 })
 
