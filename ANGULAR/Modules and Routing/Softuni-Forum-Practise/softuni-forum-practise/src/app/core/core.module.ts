bf7b8ebeb29b95c1fe307ed88eeb4d0342a3c672
Kristian Lalov <lalov.kr@gmail.com>
2023-01-15 00:01:02 +0200

    added some templated and css

--- ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/core/core.module.ts
+++ ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/core/core.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
     CoreRoutingModule
+  ],
+  exports: [ 
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ]
 })
+
+
 export class CoreModule { }
---
+++ ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/theme/theme-routing-module.ts .ts
@@ -0,0 +1,5 @@
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+export const ThemeRoutingMoudle = RouterModule.forChild(routes);
