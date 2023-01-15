1f57456fbfd6f63175407570cd34dc80c1ac0782
Kristian Lalov <lalov.kr@gmail.com>
2023-01-15 15:45:30 +0200

    fixed aside and made some changes to folders

--- ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/core/core.module.ts
+++ ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/core/core.module.ts
@@ -4,25 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    MainComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ],
-  exports: [ 
-    HeaderComponent,
-    FooterComponent,
-    MainComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, WelcomeComponent],
+  imports: [CommonModule, CoreRoutingModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-
-
-export class CoreModule { }
+export class CoreModule {}
--- ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/theme/theme.module.ts
+++ ANGULAR/Modules and Routing/Softuni-Forum-Practise/softuni-forum-practise/src/app/theme/theme.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainComponent } from './main/main.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent, RecentPostsComponent, ThemeListComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MainComponent
   ]
 })
 export class ThemeModule { }
