9bb69b5165e21514b023e9ceedae3d5e99ffe8f1
Kristian Lalov <lalov.kr@gmail.com>
2023-01-17 00:20:40 +0200

    implemented form reset

--- ANGULAR/todo-list/todo-list/src/app/todo/todo-list/todo-list.component.ts
+++ ANGULAR/todo-list/todo-list/src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { TodoService } from '../todo.service';
 
 @Component({
@@ -10,9 +11,10 @@ export class TodoListComponent {
   tasks: string[] = [];
 
   constructor(private service: TodoService) {}
+  todo = '';
 
-  addTask(value: { todo: string }) {
-    this.tasks?.push(value.todo);
-    
+  addTask(value: { todo: string }, form: NgForm) {
+    this.tasks.push(value.todo);
+    form.reset()
   }
 }
