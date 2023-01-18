09eee6031a94836d8f49b25372fc98e21c4c4c20
Kristian Lalov <lalov.kr@gmail.com>
2023-01-18 23:39:31 +0200

    Added matching passwords validator for register

--- ANGULAR/Angular Components/Workshop/forum/src/app/auth/register/register.component.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/auth/register/register.component.ts
@@ -1,28 +1,29 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { sameValueGroupValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
-  form = this.fb.group({ 
+  form = this.fb.group({
     username: [],
     email: [],
     tel: [],
     ext: [],
-    pass: this.fb.group({ 
-      password: [],
-      repass: []
-    }),
-    validators: []
-  })
+    pass: this.fb.group(
+      {
+        password: [],
+        rePassword: [],
+      },
+      {
+        validators: [sameValueGroupValidator('password', 'rePassword')],
+      }
+    ),
+  });
 
-  constructor(private fb: FormBuilder){ 
-
-  }
-
+  constructor(private fb: FormBuilder) {}
 }
