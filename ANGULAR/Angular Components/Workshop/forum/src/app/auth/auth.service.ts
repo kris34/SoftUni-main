0c68e863506228c6ab402a8d9427ddca6a063cbf
Kristian Lalov <lalov.kr@gmail.com>
2023-02-13 15:21:01 +0200

    added setProfile

--- ANGULAR/Angular Components/Workshop/forum/src/app/auth/auth.service.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/auth/auth.service.ts
@@ -1,17 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, catchError, filter, of, Subscription, tap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  filter,
+  of,
+  Subscription,
+  tap,
+  throwError,
+} from 'rxjs';
 import { IUser } from '../shared/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService implements OnDestroy {
-
   private user$$ = new BehaviorSubject<undefined | null | IUser>(undefined);
-  user$ = this.user$$.asObservable().pipe(
-    filter((val): val is IUser | null => val !== undefined)
-  );
+  user$ = this.user$$
+    .asObservable()
+    .pipe(filter((val): val is IUser | null => val !== undefined));
 
   user: IUser | null = null;
 
@@ -22,35 +29,55 @@ export class AuthService implements OnDestroy {
   subscription: Subscription;
 
   constructor(private http: HttpClient) {
-    this.subscription = this.user$.subscribe(user => {
+    this.subscription = this.user$.subscribe((user) => {
       this.user = user;
     });
   }
 
-  register(username: string, email: string, password: string, rePassword: string, tel?: string) {
-    return this.http.post<IUser>('/api/register', { username, email, password, rePassword, tel })
-      .pipe(tap(user => this.user$$.next(user)));
+  register(
+    username: string,
+    email: string,
+    password: string,
+    rePassword: string,
+    tel?: string
+  ) {
+    return this.http
+      .post<IUser>('/api/register', {
+        username,
+        email,
+        password,
+        rePassword,
+        tel,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   login(email: string, password: string) {
-    return this.http.post<any>('/api/login', { email, password })
-      .pipe(tap(user => this.user$$.next(user)));;
+    return this.http
+      .post<any>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   logout() {
-    return this.http.post<void>('/api/logout', {})
-      .pipe(tap(() => this.user$$.next(null)));;
+    return this.http
+      .post<void>('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(null)));
   }
 
   getProfile() {
-    return this.http.get<IUser>('/api/users/profile')
-      .pipe(
-        tap(user => this.user$$.next(user)),
-        catchError((err) => {
-          this.user$$.next(null);
-          return throwError(() => err);
-        })
-      );
+    return this.http.get<IUser>('/api/users/profile').pipe(
+      tap((user) => this.user$$.next(user)),
+      catchError((err) => {
+        this.user$$.next(null);
+        return throwError(() => err);
+      })
+    );
+  }
+
+  setProfile(username: string, email: string, tel?: string) {
+    return this.http
+      .put<IUser>('/api/users/profile', { username, email, tel })
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   ngOnDestroy(): void {
