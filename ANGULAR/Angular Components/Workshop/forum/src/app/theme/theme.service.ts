45eeac14f120e0f22dd7a01e6e794c887b0944fb
Kristian Lalov <lalov.kr@gmail.com>
2023-02-13 15:08:52 +0200

    created getAllThemes

--- ANGULAR/Angular Components/Workshop/forum/src/app/theme/theme.service.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/theme/theme.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ITheme } from '../shared/interfaces/themes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllThemes(){ 
+  return this.http.get<ITheme[]>('/api/themes')
+  }
+
+  ge
 }
