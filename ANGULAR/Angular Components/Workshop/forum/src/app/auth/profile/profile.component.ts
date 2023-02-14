0d101db086b83435e4f36bef81c39a1cb6645be1
Kristian Lalov <lalov.kr@gmail.com>
2023-02-14 23:55:41 +0200

    testing setProfile functionality

--- ANGULAR/Angular Components/Workshop/forum/src/app/auth/profile/profile.component.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/auth/profile/profile.component.ts
@@ -11,50 +11,45 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  showEditMode = false;
 
-  showEditMode  = false
-
-  get user() { 
-    const {username, email , tel: telephone} = this.auth.user!
-    const [ext, ...tel] = telephone.split(" ")
-    return { 
-       username,
-       email,
-       tel: tel.join(' '),
-       ext
-    }
+  get user() {
+    const { username, email, tel: telephone } = this.auth.user!;
+    const [ext, ...tel] = telephone.split(' ');
+    return {
+      username,
+      email,
+      tel: tel.join(' '),
+      ext,
+    };
   }
 
-  form = this.fb.group({ 
+  form = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
     email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
     ext: [''],
-    tel: ['']
-  })
-
+    tel: [''],
+  });
 
   constructor(private fb: FormBuilder, private auth: AuthService) {
-    this.form.setValue(this.user)
+    this.form.setValue(this.user);
   }
 
-  toggleEditMode(): void{ 
-    this.showEditMode = !this.showEditMode
+  toggleEditMode(): void {
+    this.showEditMode = !this.showEditMode;
   }
 
-  saveProfile(): void { 
-    if(this.form.invalid){ 
-      return
+  saveProfile(): void {
+    if (this.form.invalid) {
+      return;
     }
 
-    const {username, email, ext, tel } = this.form.value
+    const { username, email, ext, tel } = this.form.value;
 
-    this.auth.user = { 
-      username, email, tel: ext + ' ' + tel
-    } as any
+    this.auth.setProfile(username, email, ext + '' + tel).subscribe(() => {
+      this.toggleEditMode();
+    });
 
-    this.toggleEditMode()
-    
+    this.toggleEditMode();
   }
-
-  
 }
