e490d269b053cdaf44c91c8d8a8c86f7a3db19f0
Kristian Lalov <lalov.kr@gmail.com>
2023-02-15 00:20:19 +0200

    fixed profile edit

--- ANGULAR/Angular Components/Workshop/forum/src/app/auth/profile/profile.component.ts
+++ ANGULAR/Angular Components/Workshop/forum/src/app/auth/profile/profile.component.ts
@@ -1,55 +1,78 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { appEmailDomains } from 'src/app/shared/constants';
-import { IUser } from 'src/app/shared/interfaces';
 import { appEmailValidator } from 'src/app/shared/validators';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+
   showEditMode = false;
+  formSubmitted = false;
+
+  counter = 1;
 
   get user() {
-    const { username, email, tel: telephone } = this.auth.user!;
+    const { username, email, tel: telephone } = this.authServie.user!;
     const [ext, ...tel] = telephone.split(' ');
     return {
       username,
       email,
       tel: tel.join(' '),
-      ext,
+      ext
     };
   }
 
-  form = this.fb.group({
-    username: ['', [Validators.required, Validators.minLength(5)]],
-    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
-    ext: [''],
-    tel: [''],
-  });
+  get addresssesArray() {
+    return (this.form.get('addresses') as FormArray);
+  }
 
-  constructor(private fb: FormBuilder, private auth: AuthService) {
-    this.form.setValue(this.user);
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder, private authServie: AuthService) {
+    this.createForm({ ...this.user, addresses: [{ postCode: 'Hello', street: 'World' }] });
+  }
+
+  createForm(formValue: any) {
+    this.form = this.fb.group({
+      username: [formValue.username, [Validators.required, Validators.minLength(5)]],
+      email: [formValue.email, [Validators.required, appEmailValidator(appEmailDomains)]],
+      ext: [formValue.ext],
+      tel: [formValue.tel],
+      addresses: this.fb.array(
+        new Array(this.counter).fill(null).map((_, i) => {
+          return this.fb.group({
+            postCode: formValue.addresses[i]?.postCode || '',
+            street: formValue.addresses[i]?.street || ''
+          })
+        })
+      )
+    })
+
+  }
+
+  addNewAddress(): void {
+    this.counter++;
+    this.createForm(this.form.value);
   }
 
   toggleEditMode(): void {
     this.showEditMode = !this.showEditMode;
+    if (this.showEditMode) {
+      this.formSubmitted = false;
+    }
   }
 
   saveProfile(): void {
-    if (this.form.invalid) {
-      return;
-    }
-
+    this.formSubmitted = true;
+    if (this.form.invalid) { return; }
     const { username, email, ext, tel } = this.form.value;
-
-    this.auth.setProfile(username, email, ext + '' + tel).subscribe(() => {
+    this.authServie.setProfile(username, email, ext + ' ' + tel).subscribe(() => {
       this.toggleEditMode();
     });
-
-    this.toggleEditMode();
   }
 }
