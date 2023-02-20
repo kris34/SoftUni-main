5b6770094a2c462b0d232fd20e1240376222a7e3
Kristian Lalov <lalov.kr@gmail.com>
2023-02-20 23:10:29 +0200

    play model ready

--- nodeJS Practise/theaters/models/Play.js
+++ nodeJS Practise/theaters/models/Play.js
@@ -1,8 +1,16 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const playSchema = new Schema({
-
-    
+  title: { type: String, required: true, unique: true },
+  description: {
+    type: String,
+    required: true,
+    maxLength: [50, 'Description cannot be longer then 50 charakters!'],
+  },
+  imageUrl: { type: String, required: true },
+  isPublic: { type: Boolean, default: false },
+  createdAt: { type: String, required: true },
+  usersLiked: { type: [Types.ObjectId], ref: 'User', default: [] },
 });
 
 const Play = model('Play', playSchema);
