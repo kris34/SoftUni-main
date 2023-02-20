4822afd93b495a869a916af904fbfac0a0b3c994
Kristian Lalov <lalov.kr@gmail.com>
2023-02-20 23:48:15 +0200

    working register

--- nodeJS Practise/theaters/controllers/home.js
+++ nodeJS Practise/theaters/controllers/home.js
@@ -1,16 +1,20 @@
+const { getAllByDate } = require('../services/playService');
+
 const homeController = require('express').Router();
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
   let view;
-
+  let plays = [];
   if (req.user) {
     view = 'user-home';
+    plays = await getAllByDate();
   } else {
     view = 'guest-home';
   }
-  
+
   res.render(view, {
     title: 'Home Page',
+    plays,
   });
 });
 
