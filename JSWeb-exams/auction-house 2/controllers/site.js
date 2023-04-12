68c66d86f2516726cb56d6b6dee6061776212b4e
Kristian Lalov <lalov.kr@gmail.com>
2023-04-12 17:43:42 +0300

    working create Item

--- JSWeb-exams/auction-house 2/controllers/site.js
+++ JSWeb-exams/auction-house 2/controllers/site.js
@@ -7,6 +7,15 @@ siteController.get('/publish', async (req, res) => {
 });
 
 siteController.post('/publish', async (req, res) => {
+  const item = {
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    author: req.user._id,
+  };
+  const published = await createItem(item);
 
   res.redirect('/site/publish');
 });
