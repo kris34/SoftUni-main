cb603281730e5e73d7544537c061dfc12347ad93
Kristian Lalov <lalov.kr@gmail.com>
2023-04-12 15:25:49 +0300

    created item model

--- JSWeb-exams/auction-house 2/models/User.js
+++ JSWeb-exams/auction-house 2/models/User.js
@@ -2,13 +2,14 @@ const { Schema, model, Types } = require('mongoose');
 
 //TODO add user properties and validation according to assignment
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     unique: true,
-    minlength: [3, 'Username must be at least 3 charakters long!'],
   },
   hashedPassword: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 });
 
 userSchema.index(
---
+++ JSWeb-exams/auction-house 2/models/item.js
@@ -0,0 +1,20 @@
+const { Schema, model, Types } = require('mongoose');
+
+const URL_PATTERN = /^https?:\/\/.+$/i;
+
+const itemSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => URL_PATTERN.test(value),
+      message: 'Image not valid',
+    },
+  },
+  price: { type: Number, required: true },
+  author: { type: Types.ObjectId, ref: 'User' },
+  bidders: { type: [Types.ObjectId], ref: 'User', default: [] },
+});
